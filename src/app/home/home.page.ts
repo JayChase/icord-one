aec6ed9d58be6132b0feb55067bb15f565ff2ff8
Jonathan Chase <jonathanchase@outlook.com>
2019-09-03 12:49:11 +0900

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { from } from 'rxjs';
+import { catchError, first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  error: any;
+  location: Geoposition;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) {}
 
+  getLocation() {
+    from(this.geolocation.getCurrentPosition())
+      .pipe(
+        first(),
+        catchError(error => {
+          this.error = error;
+          return null;
+        })
+      )
+      .subscribe((result: Geoposition) => {
+        this.location = result;
+      });
+  }
 }
